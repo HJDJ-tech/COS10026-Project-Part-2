484fb3c96e115783106b39bcf7ce28cad6140320
Diesel Williams <105925975@student.swin.edu,au>
2025-05-21 15:47:23 +1000

    Linking settings.php to process_eoi.php

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -4,17 +4,8 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: apply.html");
     exit();
 }
+$mysql = require __DIR__ . "/settings.php";
 
-// Connect to database
-$host = "localhost";
-$user = "root";
-$pass = ""; // default for XAMPP
-$dbname = "project_2";                   //NOTE THIS IS FOR NEW APPLY PAGE DIRECTORY...<form method="post" action="process_eoi_project2_3.php" novalidate="novalidate">
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
 
 // Create table FIRST
 $conn->query("
