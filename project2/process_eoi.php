19f2e7adefd29a4c517f7a953cec795a8b605076
104617909 <104617909@student.swin.edu.au>
2025-05-26 21:39:56 +1000

    Testing settings.php

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -1,14 +1,6 @@
 <?php
 // Connect to database
-$host = "localhost";
-$user = "root";
-$pass = ""; // default for XAMPP
-$dbname = "project_2";
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
+require_once("settings.php");
 
 // Helper function
 function clean_input($data) {
